bbb195c69faeff64fbc0c71156524aed03352c86
Filip Was <fwas.corp@gmail.com>
2022-11-08 16:45:56 +0100

    acronym

--- javascript/acronym/acronym.js
+++ javascript/acronym/acronym.js
@@ -2,7 +2,22 @@
 // This is only a SKELETON file for the 'Acronym' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
+const TOWHITESPACE = [",", "-", "_"]
+const TONOTHING = ["'"]
+export const parse = (text = "") => {
+  for (const sign of TOWHITESPACE) {
+    text = text.replace(sign, " ")
+  }
 
-export const parse = () => {
-  throw new Error('Remove this statement and implement this function');
+  for (const sign of TONOTHING) {
+    text = text.replace(sign, "")
+  }
+
+  var returnString = ""
+  var wordsArray = text.trim().split(/\s+/)
+
+  for (const word of wordsArray) {
+    returnString += word.toUpperCase()[0]
+  }
+  return returnString;
 };

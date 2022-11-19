92e49a59f5fde7f02b36b3dce78e4eee56a96f14
Filip Was <fwas.corp@gmail.com>
2022-11-19 11:02:58 +0100

    isbn

--- javascript/isbn-verifier/isbn-verifier.js
+++ javascript/isbn-verifier/isbn-verifier.js
@@ -3,6 +3,21 @@
 // convenience to get you started writing code faster.
 //
 
-export const isValid = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isValid = (number) => {
+  number = number.replace(/\W/g,"").split("")
+
+  if (number.length != 10) return false
+
+  let sum = number.reduce((acc, curr, idx, arr) => {
+    
+    if (curr == "X" && idx == 9) {
+      curr = 10
+    } else if (isNaN(curr)) {
+      return 0.5
+    }
+
+    return acc += (10-idx)*curr
+  }, 0)
+
+  return sum % 11 == 0 ? true : false
 };

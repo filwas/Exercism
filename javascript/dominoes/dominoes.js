9d0fb64229b7b63a85498428e527789f3a1120f9
Filip Wąs <fwas.corp@gmail.com>
2023-04-01 16:00:26 +0200

    domino

---
+++ javascript/dominoes/dominoes.js
@@ -0,0 +1,11 @@
+//
+// This is only a SKELETON file for the 'Dominoes' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const chain = (dominoes) => {
+  let length = dominoes.length;
+  console.log(dominoes);
+
+  return dominoes
+};

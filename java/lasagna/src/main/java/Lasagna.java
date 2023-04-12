47ee2d50d4e6ecafed192267424697b4fd6450f7
Filip Was <fwas.corp@gmail.com>
2023-04-12 14:53:52 +0200

    gna

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,19 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
+    
+    public int expectedMinutesInOven() {
+        return 40;
+    }
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int hasBeen) {
+        return expectedMinutesInOven() - hasBeen;
+    }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers) {
+        return 2*layers;
+    }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers, int hasBeen) {
+        return hasBeen + preparationTimeInMinutes(layers);
+    }
+
 }

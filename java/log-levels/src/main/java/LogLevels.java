1bc5449a76e4e64914164bdada3fcfa52096a41e
Filip Was <fwas.corp@gmail.com>
2023-04-12 22:45:24 +0200

    asdf

---
+++ java/log-levels/src/main/java/LogLevels.java
@@ -0,0 +1,16 @@
+public class LogLevels {
+    
+    public static String message(String logLine) {
+
+        String str = "[ERROR]: Invalid operation";
+        return str;
+    }
+
+    public static String logLevel(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLine.logLevel() method");
+    }
+
+    public static String reformat(String logLine) {
+        throw new UnsupportedOperationException("Please implement the (static) LogLine.reformat() method");
+    }
+}

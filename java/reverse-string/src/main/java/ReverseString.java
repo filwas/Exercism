15e7c7ac96d188badf970eb715d92a6271ec2f4a
Filip Was <fwas.corp@gmail.com>
2023-04-13 20:10:55 +0200

    ReverseString

---
+++ java/reverse-string/src/main/java/ReverseString.java
@@ -0,0 +1,18 @@
+class ReverseString {
+
+    String reverse(String inputString) {
+        
+        char[] charArr = inputString.toCharArray();
+        int charLen = charArr.length;
+        String[] retArr = new String[charLen];
+
+        for (char c : charArr) {
+            charLen--;
+            retArr[charLen] = ""+c;
+            System.out.println(retArr[charLen]);
+        }
+        String retString = String.join("", retArr);
+        return retString;
+    }
+
+}

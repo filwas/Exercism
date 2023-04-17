3f1475a815702b554189823e33ebbea97ace855c
Filip Was <fwas.corp@gmail.com>
2023-04-17 18:28:16 +0200

    need

---
+++ java/elons-toy-car/src/main/java/ElonsToyCar.java
@@ -0,0 +1,23 @@
+public class ElonsToyCar {
+    private int distance;
+    private int battery = 100;
+
+    public static ElonsToyCar buy() {
+        return new ElonsToyCar();
+    }
+
+    public String distanceDisplay() {
+        return "Driven " + this.distance + " meters";
+    }
+
+    public String batteryDisplay() {
+        return this.battery == 0 ? "Battery empty" : "Battery at " + this.battery + "%";
+    }
+
+    public void drive() {
+        if (this.battery > 0) {
+            this.distance += 20;
+            this.battery --;
+        }
+    }
+}
